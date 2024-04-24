0bf10d63e5abeff0c468743598c4c08c4ad26f93
Oriol Gali <oriol.gali.7e6@itb.cat>
2024-04-24 17:47:21 +0200

    scripts para crear el grid

---
+++ Assets/Scripts/PathNode.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathNode : MonoBehaviour
+{
+    /*private Grid<PathNode> _grid;
+    private int _x;
+    private int _y;
+
+    public int gCost;
+    public int hCost;
+    public int fCost;
+
+    public PathNode cameFromNode;
+
+    public PathNode(Grid<PathNode> grid, int x, int y)
+    {
+        this._grid = grid;
+        this._x = x;
+        this._y = y;
+    }
+
+    public override string ToString()
+    {
+        return _x + "," + _y;
+    }*/
+}
---
+++ Assets/Scripts/Pathfinding.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinding
+{
+    //private Grid<PathNode> _grid;
+    public Pathfinding(int width, int height)
+    {
+        //_grid = new Grid<PathNode>(width, height, 10f, Vector3.zero, (Grid<PathNode> g, int x, int y) => new PathNode(g, x, y));
+    }
+}
