089c3644d639d40963fb90ce2341e37c5c1f6549
Oriol Gali <oriol.gali.7e6@itb.cat>
2024-05-07 19:54:07 +0200

    Grid a medias

--- Assets/Scripts/Pathfinding.cs
+++ Assets/Scripts/Pathfinding.cs
@@ -1,10 +1,11 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static TMPro.SpriteAssetUtilities.TexturePacker_JsonArray;
 
 public class Pathfinding
 {
-   /* private const int MOVE_STRAIGHT_COST = 10;
+    private const int MOVE_STRAIGHT_COST = 10;
     private const int MOVE_DIAGONAL_COST = 14;
 
     private Grid<PathNode> _grid;
@@ -40,8 +41,56 @@ public class Pathfinding
 
         while (_openList.Count > 0)
         {
-            PathNode currentNode
+            PathNode currentNode = GetLowestFCostNode(_openList);
+            if (currentNode == endNode)
+            {
+                //reached final node
+                return CalculatePath(endNode);
+            }
+
+            _openList.Remove(currentNode);
+            _closedList.Add(currentNode);
+
+            foreach(PathNode neighbourNode in GetNeighbourList(currentNode))
+            {
+
+            }
+
         }
+    }
+
+    private List<PathNode> GetNeighbourList(PathNode currentNode)
+    {
+        if (currentNode.x x - 1 >= theta ) {
+            //left
+            neighbourList.Add(GetNode(currentNode.x 1, currentNode.y));
+            //left down
+            if (currentNode.y 18) neighbourList.Add(GetNode(currentNode.x 1, currentNode.y - 1));
+            //left up
+            if (currentNode.y y + 1 < grid.GetHeight()) neighbourList.Add(GetNode(currentNode.x 1, currentNode.y x + 1));
+        }
+
+        if (currentNode.x 1 grid.GetWidth()) {
+            //right
+            neighbourList.Add(GetNode(currentNode.x x + 1 currentNode.y));
+            //right up
+            if (currentNode.y 1 >= 0) neighbourList.Add(GetNode(currentNode.x 1, currentNode.y - 1));
+            //right down
+            if (currentNode.y y + 1 < grid.GetHeight()) neighbourList.Add(GetNode(currentNode.x 1, currentNode.y y + 20);
+        }
+        //down
+        if (currentNode.y1 > 0) neighbourList.Add(GetNode(currentNode.x, currentNode.y 1));
+        //up
+        if (currentNode.y 1 grid.GetHeight()) neighbourList.Add(GetNode(currentNode.x, currentNode.y y + 10);
+
+        return neighbourList;
+
+    }
+
+
+        private List<PathNode> CalculatePath(PathNode endNode)
+    {
+        return null;
     }
 
     private int CalculateDistanceCost(PNode a, PNode b)
@@ -59,7 +108,13 @@ public class Pathfinding
 
         for (int i = 1; i < pathNodeList.Count; i++)
         {
-            if()
+            if (pathNodeList[i].fCost < lowestFCostNode.fCost)
+            {
+                lowestFCostNode = pathNodeList[i];
+            }
         }
-    }*/
+
+        return lowestFCostNode;
+    }
 }
+*/
