8f98ad2c05bad5b4225e153e625ec0d1e9cdd716
Oriol Gali <oriol.gali.7e6@itb.cat>
2024-05-08 17:47:20 +0200

    Añadir scripts

--- Assets/Scripts/PathNode.cs
+++ Assets/Scripts/PathNode.cs
@@ -1,28 +1,50 @@
+﻿/* 
+    ------------------- Code Monkey -------------------
+
+    Thank you for downloading this package
+    I hope you find it useful in your projects
+    If you have any questions let me know
+    Cheers!
+
+               unitycodemonkey.com
+    --------------------------------------------------
+ */
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PathNode
-{
-/*    private Grid<PathNode> _grid;
-    private int _x;
-    private int _y;
+public class PathNode {
+
+    private Grid<PathNode> grid;
+    public int x;
+    public int y;
 
     public int gCost;
     public int hCost;
     public int fCost;
 
+    public bool isWalkable;
     public PathNode cameFromNode;
 
-    public PathNode(Grid<PathNode> grid, int x, int y)
-    {
-        _grid = grid;
-        _x = x;
-        _y = y;
+    public PathNode(Grid<PathNode> grid, int x, int y) {
+        this.grid = grid;
+        this.x = x;
+        this.y = y;
+        isWalkable = true;
     }
 
-    public override string ToString()
-    {
-        return _x + "," + _y;
-    }*/
+    public void CalculateFCost() {
+        fCost = gCost + hCost;
+    }
+
+    public void SetIsWalkable(bool isWalkable) {
+        this.isWalkable = isWalkable;
+        grid.TriggerGridObjectChanged(x, y);
+    }
+
+    public override string ToString() {
+        return x + "," + y;
+    }
+
 }
